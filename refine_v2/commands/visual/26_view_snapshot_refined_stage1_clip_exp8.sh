ada023f9a6cb74aeb85be3cc6b2aa5f93b8434b6
Keeewl <2510673021@mails.szu.edu.cn>
2026-04-26 22:37:07 +0800

    Local: figure2_framework

--- refine_v2/commands/visual/26_view_snapshot_refined_stage1_clip_exp8.sh
+++ refine_v2/commands/visual/26_view_snapshot_refined_stage1_clip_exp8.sh
@@ -5,7 +5,14 @@ python visualize/viewer/snapshot_viewer.py \
   --dataset interx \
   --data_dir refine_v2/save/infer/refiner_v2_exp8_on_stage1_clip_0001_handshake/refined \
   --clip_name 0001_Handshake \
-  --frame_ids 0 14 79 91 \
+  --frame_ids 0 14 69 91 \
   --offset_dir 0 0 1 \
   --spacing 2.0 \
   --time_gradient
+
+cd visualize/viewer
+python data_viewer.py \
+  --dataset interx \
+  --data_dir ../../refine_v2/save/infer/refiner_v2_exp8_on_stage1_clip_0001_handshake/refined \
+  --texts_dir '' \
+  --title 'stage1-clip-refined-exp8'
